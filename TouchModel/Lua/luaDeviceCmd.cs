0ea519fdbcef3d18ac7fa06d845e486baa2bff6c
songmou <771419021@qq.com>
2016-10-15 12:55:46 +0800

    git为什么没有上传

---
+++ TouchModel/Lua/luaDeviceCmd.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchModel.Lua
+{
+    /// <summary>
+    /// 和手机脚本请求有关的任务Model
+    public class luaDeviceCmd
+    {
+        public luaDeviceCmd()
+        {
+            this.type = luaCmdType.wait.ToString();
+            this.text = "";
+            this.title = "等待命令";
+            this.status = "等待命令";
+            this.lastDate = DateTime.Now;
+        }
+        public string type { get; set; }
+
+        /// <summary>
+        /// 命令的文件名或文本
+        /// </summary>
+        public string text { get; set; }
+        public string title { get; set; }
+        public string status { get; set; }
+        public DateTime lastDate { get; set; }
+    }
+
+    public enum luaCmdType
+    {
+        wait,
+        cmd,
+        text,
+        exit,
+        time
+    }
+}
