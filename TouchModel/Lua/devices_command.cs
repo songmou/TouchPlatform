21511565650899c38874a0702b3a8d54a2d9d991
songmou <771419021@qq.com>
2016-10-15 12:56:29 +0800

    .....

---
+++ TouchModel/Lua/devices_command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchModel.Lua
+{
+    /// <summary>
+    /// 手机要执行的脚本任务表
+    /// </summary>
+    public class devices_command : Entity
+    {
+        public string deviceid { get; set; }
+        public string luatype { get; set; }
+        public string luaname { get; set; }
+        public string luatitle { get; set; }
+        public string status { get; set; }
+    }
+}
